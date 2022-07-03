92fc8349c0e94d59956d2c0f5d46093e53381146
Nguyen Huy Anh <anhnh13@gmail.com>
2022-07-03 21:21:04 +0700

    complete ticket modal

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 const buyBtns = document.querySelectorAll('.js-buy-ticket');
+const modal = document.querySelector('.js-modal');
+const modalClose = document.querySelector('.js-modal-close');
+const modalContainer = document.querySelector('.js-modal-container');
+// loop through buttons
+for (const buyBtn of buyBtns) {
+    buyBtn.addEventListener('click', showBuyTickets)
+}
+
+modalClose.addEventListener('click', hideBuyTickets);
+
+modal.addEventListener('click', hideBuyTickets);
+modalContainer.addEventListener('click', function (event) {
+    event.stopPropagation();
+});
+// function to show and hide modal (buy tickets)
+function showBuyTickets () {
+    modal.classList.add('open');
+}
+function hideBuyTickets () {
+    modal.classList.remove('open');
+}
